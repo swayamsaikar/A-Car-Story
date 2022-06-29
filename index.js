f7ab31bacf8e69b88a2783373ca8abeee72d8a1c
Swayam Sai Kar <64484896+swayamsaikar@users.noreply.github.com>
2022-06-29 22:49:46 +0530

    The rotation increases when being clicked - Sports Car

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent("base", {
   schema: {
     radius: {
       type: "number",
-      default: 1000,
+      default: 100,
     },
     height: {
       type: "number",
@@ -20,7 +20,52 @@ AFRAME.registerComponent("base", {
       height: this.data.height,
     });
 
-    this.el.setAttribute("material", { color: "#1769aa" });
     // console.log("ended")
+  },
+});
+
+AFRAME.registerComponent("change-rotation", {
+  schema: {
+    clickCounter: {
+      type: "number",
+      default: 0,
+    },
+  },
+
+  update: function () {
+    // Do something when component's data is updated.
+    window.addEventListener("click", (e) => {
+      this.data.clickCounter = this.data.clickCounter + 1;
+      if (this.data.clickCounter === 1) {
+        let rotation = {
+          x: -90,
+          y: 100,
+          z: 0,
+        };
+        this.el.setAttribute("rotation", rotation);
+      } else if (this.data.clickCounter === 2) {
+        let rotation = {
+          x: -90,
+          y: 180,
+          z: 0,
+        };
+        this.el.setAttribute("rotation", rotation);
+      } else if (this.data.clickCounter === 3) {
+        let rotation = {
+          x: -90,
+          y: -90,
+          z: 0,
+        };
+        this.el.setAttribute("rotation", rotation);
+      } else {
+        let rotation = {
+          x: -90,
+          y: 40,
+          z: 0,
+        };
+        this.el.setAttribute("rotation", rotation);
+        this.data.clickCounter = 0;
+      }
+    });
   },
 });
